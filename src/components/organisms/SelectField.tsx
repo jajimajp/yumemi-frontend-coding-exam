3211347faa773c27bc49f55ca59f1288c67a6ca7
Yajima Soichi <jajima.jp@gmail.com>
2023-11-17 18:29:26 +0900

    feat: onChangeに現在選択中の選択肢一覧を渡す

--- src/components/organisms/SelectField.tsx
+++ src/components/organisms/SelectField.tsx
@@ -1,28 +1,41 @@
+import { useState } from 'react'
 import Fieldset from '../atoms/Fieldset'
 import Legend from '../atoms/Legend'
-import CheckboxWithLabel from '../molecules/CheckboxWithLabel'
+import CheckboxWithLabel, { CheckboxWithLabelChangeEvent } from '../molecules/CheckboxWithLabel'
 
 type Prop = {
   // フィールドのタイトル ラベルとして表示される
   title: string
   // 選択肢
   options: SelectFieldOption[]
-  onChange?: React.ComponentProps<typeof CheckboxWithLabel>['onChange']
+  onChange?: (event: SelectFieldChangeEvent) => void
 }
 
 type SelectFieldOption = {
-  value: string
+  value: React.ComponentProps<typeof CheckboxWithLabel>['value']
   label: string
+}
+
+type SelectFieldChangeEvent = {
+  target: CheckboxWithLabelChangeEvent
+  selectedOptions: SelectFieldOption[]
 }
 
 /**
  * 複数のオプションから選択を行うフィールド
  */
 export default function SelectField({ title, options, onChange }: Prop) {
+  const [selectedOptions, setSelectedOptions] = useState<SelectFieldOption[]>([])
+
   const handleChangeCheckbox: React.ComponentProps<typeof CheckboxWithLabel>['onChange'] = (
     event,
   ) => {
-    onChange && onChange(event)
+    const newSelectedOptions = event.checked
+      ? [...selectedOptions, { value: event.value, label: event.label }] // 選択中の選択肢リストに追加
+      : selectedOptions.filter((option) => option.value !== event.value) // 選択中の選択肢リストから削除
+
+    onChange && onChange({ target: event, selectedOptions: newSelectedOptions })
+    setSelectedOptions(newSelectedOptions)
   }
 
   return (
@@ -31,7 +44,7 @@ export default function SelectField({ title, options, onChange }: Prop) {
       {options.map((option) => {
         return (
           <CheckboxWithLabel
-            key={option.value}
+            key={String(option.value)}
             value={option.value}
             label={option.label}
             onChange={handleChangeCheckbox}
